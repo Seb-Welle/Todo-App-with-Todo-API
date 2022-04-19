33069d87edcef8885d29b45fd923fee9e0a26126
Sebastian Welle <s-welle@web.de>
2022-04-19 17:00:39 +0200

    feat: adds function to add, change and to delete done Todos

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const addTodoBtn = document.querySelector("#new-todo-btn");
+const removeDoneBtn = document.querySelector("#remove-done-btn");
 const newTodoInput = document.querySelector("#new-todo");
 const todoList = document.querySelector("#list");
 let todos = [];
 
 function loadTodos() {
-  fetch("http://localhost:4730/todos")
+  fetch("http://localhost:4730/todos/")
     .then((response) => response.json())
     .then((todosFromApi) => {
-      console.log(todosFromApi);
+      //console.log(todosFromApi);
       todos = todosFromApi;
       renderTodos();
     });
@@ -15,14 +16,41 @@ function loadTodos() {
 
 function renderTodos() {
   todoList.innerHTML = "";
+
   todos.forEach((todos) => {
     const newLi = document.createElement("li");
     const text = document.createTextNode(todos.description);
     newLi.appendChild(text);
 
+    newLi.setAttribute("data-id", todos.id);
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todos.done;
+    newLi.appendChild(checkbox);
+
     todoList.appendChild(newLi);
   });
 }
+
+todoList.addEventListener("change", (e) => {
+  //String zu Number konvertieren!
+  const todoId = Number(e.target.parentElement.dataset.id);
+  const todoDescription = todos.find((todo) => todo.id === todoId).description;
+  console.log(todoDescription);
+  const todoCheckboxChecked = e.target.checked;
+
+  const updatedTodo = {
+    description: todoDescription,
+    done: todoCheckboxChecked,
+  };
+
+  fetch("http://localhost:4730/todos/" + todoId, {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(updatedTodo),
+  }).then(() => loadTodos());
+});
 
 addTodoBtn.addEventListener("click", () => {
   const newTodoText = newTodoInput.value;
@@ -30,18 +58,29 @@ addTodoBtn.addEventListener("click", () => {
     description: newTodoText,
     done: false,
   };
-  fetch("http://localhost:4730/todos", {
+  fetch("http://localhost:4730/todos/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newTodo),
   })
-    .then(response.json())
+    .then((response) => response.json())
     .then((newTodoFromApi) => {
       todos.push(newTodoFromApi);
       renderTodos();
     });
+});
+
+removeDoneBtn.addEventListener("click", () => {
+  todos.forEach((todos) => {
+    if (todos.done) {
+      fetch("http://localhost:4730/todos/" + todos.id, {
+        method: "DELETE",
+      });
+    }
+  });
+  loadTodos();
 });
 
 loadTodos();
